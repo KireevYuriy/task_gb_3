2503fba71293c2c618b556786f7395a9578d2bc0
Yuriy <kireev_juriyy@rambler.ru>
2021-05-21 00:43:36 +0800

    writing the body of the programm in task_3_3

---
+++ task_3_3.py
@@ -0,0 +1,27 @@
+names = ['Игорь','Александр','Инна','Владимир','Анна','Иван','Данила','Роман','Анастасия','Дарья']
+letter_names = {}
+
+
+def thesaurus(letter_key, list_names):
+    """
+    Функция проверяет первую букву имени со всеми именами, если имен несколько, формирует список из этих имен,
+    если имя одно, то список из одного имени. Функция возвращает букву и список имен на эту букву.
+    """
+    same_name = []
+    for check_name in list_names:
+        if letter_key == check_name[0]:
+            same_name.append(check_name)
+    return (letter_key, same_name)
+
+
+for name in names:
+    key,val = thesaurus(name[0], names)
+    letter_names.setdefault(key, val)
+print(letter_names)
+
+
+
+
+
+
+
